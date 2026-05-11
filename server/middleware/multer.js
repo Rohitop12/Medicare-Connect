5b2f9627e2b4d3211a5a65152f51ffea63b447d3
Rohit Kumar <your-email@gmail.com>
2026-05-12 01:00:30 +0530

    refactor: remove CloudinaryStorage, use simple multer disk storage, clean cloudinary config

--- server/middleware/multer.js
+++ server/middleware/multer.js
@@ -1,16 +1,6 @@
+
 const multer = require('multer');
-const { CloudinaryStorage } = require('multer-storage-cloudinary');
-const cloudinary = require('../config/cloudinary');
-
-const storage = new CloudinaryStorage({
-  cloudinary: cloudinary,
-  params: {
-    folder: 'medicare_profiles',
-    allowedFormats: ['jpg', 'png', 'jpeg', 'webp'],
-    transformation: [{ width: 500, height: 500, crop: 'limit' }]
-  }
-});
-
-const upload = multer({ storage: storage });
+const storage = multer.diskStorage({});
+const upload = multer({ storage });
 
 module.exports = upload;
